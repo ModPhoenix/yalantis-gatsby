4cc3935e6fb408d8c94b5c2d9af8637ab244a41d
Bohdan Ivanchenko <bohdan.ivanchenko@yalantis.com>
2020-03-16 10:54:26 +0200

    Add fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,15 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        custom: {
+          families: ["BrandonText, Radikal"],
+          urls: ["/fonts/fonts.css"],
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
